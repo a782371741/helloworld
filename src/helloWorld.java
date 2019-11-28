836856ee09f53ce0079a7cfaf5bd2000fd287088
Administrator <782371741@qq.com>
2019-11-28 17:14:58 +0800

    test

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -2,7 +2,5 @@ public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!!!!!!!!!!!!!!!!!");
         System.out.println("nihao");
-        System.out.println("xn");
-        System.out.println("zjj");
     }
 }
